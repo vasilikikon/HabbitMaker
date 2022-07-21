2689ceb81c7d1d5ecc5c1321304b373c3850c595
vasilikikon <vasia.konstanti@gmail.com>
2022-07-21 02:12:54 +0200

    cntAll
    trying to create the function in javascript that is gonna count all the elements in my ol list

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ const Buttons = [
 var today = new Date();
 var date = today.getDate() +'/'+(today.getMonth()+1) +'/' + today.getFullYear();
 document.getElementById("date").innerHTML = date;
+
+
+
+var ol = document.getElementById("ListOfCategories");
+var liNodes = [];
+for(var i= 0; i<ol.childNodes.length; i++){
+   if(ol.childNodes[i] == "li"){
+      liNodes.push(ol.childNodes[i]);
+      console.log(liNodes);
+   }
+}
+
+
