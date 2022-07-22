1e8376d7ac0093d8748aa940f08453f60bfa073e
Vasiliki Konstanti <vasia.konstanti@gmail.com>
2022-07-23 01:18:09 +0200

    open button
    starting to work on the open button
    added an eventListener

--- app.js
+++ app.js
@@ -13,15 +13,19 @@ const Buttons = [
  }
 ]
 
+//gives the date
 var today = new Date();
 var date = today.getDate() +'/'+(today.getMonth()+1) +'/' + today.getFullYear();
 document.getElementById("date").innerHTML = date;
 
-
-
+//counts categories
 var olCNTCategories = document.getElementById("ListOfCategories").childElementCount;
 console.log(olCNTCategories);
 document.getElementById("cntAll").innerHTML = olCNTCategories;
 
+//interaction with open button todo
+document.getElementById("open").addEventListener('click',function(){
+console.log("the open is clicked");
+})
 
 
