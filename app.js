e188af4a919f7e81b1ea10cf4131b7ad65c8dff0
Vasiliki Konstanti <vasia.konstanti@gmail.com>
2022-07-23 01:03:20 +0200

    olCNTCategories
    made the cnt for the amount of categories

--- app.js
+++ app.js
@@ -19,13 +19,9 @@ document.getElementById("date").innerHTML = date;
 
 
 
-var ol = document.getElementById("ListOfCategories");
-var liNodes = [];
-for(var i= 0; i<ol.childNodes.length; i++){
-   if(ol.childNodes[i] == "li"){
-      liNodes.push(ol.childNodes[i]);
-      console.log(liNodes);
-   }
-}
+var olCNTCategories = document.getElementById("ListOfCategories").childElementCount;
+console.log(olCNTCategories);
+document.getElementById("cntAll").innerHTML = olCNTCategories;
+
 
 
