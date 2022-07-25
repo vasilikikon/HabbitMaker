a4ff237bae1e7ada73e46f76cbbd8cc0249bd7c4
Vasiliki Konstanti <vasia.konstanti@gmail.com>
2022-07-25 23:18:44 +0200

    create a untegroup after pressing the create button
    working on /create a untegroup after pressing the create button
    PRIO 1

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
-const Buttons = [
- {
-  name:'create',
-  img:'images\ADD.jpg'
- },
- {
-  name:'delete',
-  img:'images\DELETE.jpg'
- },
- {
-    name:'open',
-    img:'images\open.png'
- }
-]
+const buttons = [
+  {
+    name: "create",
+    img: "imagesADD.jpg",
+  },
+  {
+    name: "delete",
+    img: "imagesDELETE.jpg",
+  },
+  {
+    name: "open",
+    img: "imagesopen.png",
+  },
+];
+const categories = [
+  {
+    nameCateg: "",
+  },
+];
+
+const ListCat = document.getElementById("ListOfCategories");
 
 //gives the date
 var today = new Date();
-var date = today.getDate() +'/'+(today.getMonth()+1) +'/' + today.getFullYear();
+var date =
+  today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
 document.getElementById("date").innerHTML = date;
 
 //counts categories
-var olCNTCategories = document.getElementById("ListOfCategories").childElementCount;
+var olCNTCategories =
+  document.getElementById("ListOfCategories").childElementCount;
 console.log(olCNTCategories);
 document.getElementById("cntAll").innerHTML = olCNTCategories;
 
+//create a untegroup after pressing the create button
+document.getElementById("create").addEventListener("click", function () {
+  const input = "HI";
+  if (categories.nameCateg !== input) {
+    var el = document.createElement("ul");
+    ListCat.appendChild(el);
+  }
+});
+
 //interaction with open button todo
-document.getElementById("open").addEventListener('click',function(){
-console.log("the open is clicked");
-})
-
-
+document.getElementById("open").addEventListener("click", function () {
+  console.log("the open is clicked");
+});
