f3b2433e67f5fad654cf9ce57d1f5dd4c6df9990
Vasiliki Konstanti <vasia.konstanti@gmail.com>
2022-08-13 12:37:11 +0300

    touch ups
    continue the Category form thing

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const tasksId = document.getElementById("ListOfTasks");
 const catId = document.getElementById("ListOfCat");
 const btnAdd = document.getElementById("add");
 const btnDelete = document.getElementById("delete");
-
+const btnSubmit = document.getElementById("submit");
 //gives the date
 function getDate() {
   var today = new Date();
@@ -63,35 +63,36 @@ function createIdOpen() {
 
 //if you press the ADD button
 btnAdd.addEventListener("click", function () {
-  // Create LiCatElement
-  const newCat = document.createElement("li");
-  // Create the openButton for the element and the img for it
-  const openBu = document.createElement("button");
-  const openBuImg = document.createElement("img");
-  //  Set ID attribute on element and the button
-  var idforCat = createIdLiCat();
-  newCat.setAttribute("id", idforCat);
-  openBu.setAttribute("id", createIdOpen());
-  openBu.setAttribute("type", "button");
-  openBuImg.setAttribute("src", "images/open.png");
-  openBuImg.setAttribute("alt", "open up");
-  // Add text content to element
   window.location.href = "newCategoryForm.html";
-  /*var newCatName = window.prompt("Name the category for new task:");
+  btnSubmit.addEventListener("click", function () {
+    // Create LiCatElement
+    const newCat = document.createElement("li");
+    // Create the openButton for the element and the img for it
+    const openBu = document.createElement("button");
+    const openBuImg = document.createElement("img");
+    //  Set ID attribute on element and the button
+    var idforCat = createIdLiCat();
+    newCat.setAttribute("id", idforCat);
+    openBu.setAttribute("id", createIdOpen());
+    openBu.setAttribute("type", "button");
+    openBuImg.setAttribute("src", "images/open.png");
+    openBuImg.setAttribute("alt", "open up");
+    // Add text content to element
+    /*var newCatName = window.prompt("Name the category for new task:");
   newCat.innerHTML = newCatName;
   */
-  //  add element to DOM
-  catId.appendChild(newCat);
-  newCat.appendChild(openBu);
-  openBu.appendChild(openBuImg);
-  //add new cat to the categories array
-  categories.push({ newCatName, idforCat });
-  console.log(categories);
-  //counts categories and update the title
-  var cntCat = catId.childElementCount;
-  document.getElementById("cntCat").innerHTML = cntCat;
+    //  add element to DOM
+    catId.appendChild(newCat);
+    newCat.appendChild(openBu);
+    openBu.appendChild(openBuImg);
+    //add new cat to the categories array
+    categories.push({ newCatName, idforCat });
+    console.log(categories);
+    //counts categories and update the title
+    var cntCat = catId.childElementCount;
+    document.getElementById("cntCat").innerHTML = cntCat;
+  });
 });
-
 //interaction with open button todo
 /*document.getElementById("open").addEventListener("click", function () {
   console.log("the open is clicked");
