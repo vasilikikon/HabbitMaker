6dc125a6be871e3124b79fc41ad679f65248ab19
Vasiliki Konstanti <vasia.konstanti@gmail.com>
2022-08-20 10:02:21 +0300

    changed up thigs so that 2 js work together
    changed up thigs so that 2 js work together and not interfier in non wanted ways.

--- app.js
+++ app.js
@@ -37,18 +37,27 @@ const catId = document.getElementById("ListOfCat");
 const btnAdd = document.getElementById("add");
 const btnDelete = document.getElementById("delete");
 const btnSubmit = document.getElementById("submit");
-//gives the date
-function getDate() {
-  var today = new Date();
-  var date =
-    today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
-  document.getElementById("date").textContent = date;
+if (document.getElementById("date")) {
+  //gives the date
+  function getDate() {
+    var today = new Date();
+    var date =
+      today.getDate() +
+      "/" +
+      (today.getMonth() + 1) +
+      "/" +
+      today.getFullYear();
+    document.getElementById("date").textContent = date;
+  }
+  getDate();
+  //counts categories and outputs in the title
+  var cntCat = catId.childElementCount;
+  document.getElementById("cntCat").innerHTML = cntCat;
+  //if you press the ADD button
+  btnAdd.addEventListener("click", function () {
+    window.location.href = "newCategoryForm.html";
+  });
 }
-getDate();
-
-//counts categories and outputs in the title
-var cntCat = catId.childElementCount;
-document.getElementById("cntCat").innerHTML = cntCat;
 
 //id for the Categories
 var idCat = 0;
@@ -61,39 +70,8 @@ function createIdOpen() {
   return "o" + idOpen++;
 }
 
-//if you press the ADD button
-btnAdd.addEventListener("click", function () {
-  window.location.href = "newCategoryForm.html";
-});
-btnSubmit.addEventListener("click", function () {
-  // Create LiCatElement
-  const newCat = document.createElement("li");
-  // Create the openButton for the element and the img for it
-  const openBu = document.createElement("button");
-  const openBuImg = document.createElement("img");
-  //  Set ID attribute on element and the button
-  var idforCat = createIdLiCat();
-  newCat.setAttribute("id", idforCat);
-  openBu.setAttribute("id", createIdOpen());
-  openBu.setAttribute("type", "button");
-  openBuImg.setAttribute("src", "images/open.png");
-  openBuImg.setAttribute("alt", "open up");
-  // Add text content to element
-  /*var newCatName = window.prompt("Name the category for new task:");
-  newCat.innerHTML = newCatName;
-  */
-  //  add element to DOM
-  catId.appendChild(newCat);
-  newCat.appendChild(openBu);
-  openBu.appendChild(openBuImg);
-  //add new cat to the categories array
-  categories.push({ newCatName, idforCat });
-  console.log(categories);
-  //counts categories and update the title
-  var cntCat = catId.childElementCount;
-  document.getElementById("cntCat").innerHTML = cntCat;
-  window.location.href = "index.html";
-});
+//export { categories };
+
 //interaction with open button todo
 /*document.getElementById("open").addEventListener("click", function () {
   console.log("the open is clicked");
