9dd29da5ccddcb339b4ce6fa0ca6bf32cd54485b
Vasiliki Konstanti <vasia.konstanti@gmail.com>
2022-09-12 21:44:30 +0200

    added a tag
    idea for next time: just try the get and set thing on some new plain html files

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ if (document.getElementById("date")) {
   var cntCat = catId.childElementCount;
   document.getElementById("cntCat").innerHTML = cntCat;
   //if you press the ADD button
-  btnAdd.addEventListener("click", function () {
+  /* btnAdd.addEventListener("click", function () {
     window.location.href = "newCategoryForm.html";
-  });
+  });*/
 }
 
 //id for the Categories
@@ -71,7 +71,36 @@ function createIdOpen() {
 }
 
 //export { categories };
-
+console.log(categories);
+btnSubmit.addEventListener("click", function () {
+  // Create LiCatElement
+  const newCat = document.createElement("li");
+  // Create the openButton for the element and the img for it
+  const openBu = document.createElement("button");
+  const openBuImg = document.createElement("img");
+  //  Set ID attribute on element and the button
+  var idforCat = createIdLiCat();
+  newCat.setAttribute("id", idforCat);
+  openBu.setAttribute("id", createIdOpen());
+  openBu.setAttribute("type", "button");
+  openBuImg.setAttribute("src", "images/open.png");
+  openBuImg.setAttribute("alt", "open up");
+  // Add text content to element
+  /*var newCatName = window.prompt("Name the category for new task:");
+  newCat.innerHTML = newCatName;
+  */
+  //  add element to DOM
+  catId.appendChild(newCat);
+  newCat.appendChild(openBu);
+  openBu.appendChild(openBuImg);
+  //add new cat to the categories array
+  categories.push({ newCatName, idforCat });
+  console.log(categories);
+  //counts categories and update the title
+  var cntCat = catId.childElementCount;
+  document.getElementById("cntCat").innerHTML = cntCat;
+  window.location.href = "index.html";
+});
 //interaction with open button todo
 /*document.getElementById("open").addEventListener("click", function () {
   console.log("the open is clicked");
